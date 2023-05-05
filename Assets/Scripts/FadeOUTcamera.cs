37ccf0d1810be2e7dcb3df50a3271b258161cfad
kamatej <kamila.michalak2014@gmail.com>
2023-05-05 17:42:53 +0100

    fade in and out tested - all good
    fade in - on the train 4s from black;
    fade out - after 195seconds - the end of the experience - fade out script - from the train to the black

---
+++ Assets/Scripts/FadeOUTcamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeOUTcamera : MonoBehaviour
+{
+    [Range(1, 0)] public float alpha;
+    public Image image;
+
+    void Start()
+    {
+        StartCoroutine(FadeOutAfterTime(195f)); // Start coroutine to fade out after 3 minutes and 15 seconds (195 seconds)
+    }
+
+    IEnumerator FadeOutAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Fade out the scene
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime / 2; // Adjust the fade speed by changing the divisor (e.g. divide by 4 for a slower fade)
+            var tempColor = image.color;
+            tempColor.a = alpha;
+            image.color = tempColor;
+            yield return null;
+        }
+
+        // Load a new scene or quit the game
+        // In this example, we'll just quit the game
+        Application.Quit();
+    }
+}
